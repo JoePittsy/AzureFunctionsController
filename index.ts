15ffe717e4f9ca5077c6e6be7f7c07bae48b9a8a
Joe Pitts <JPitts@TrueNorthIT.co.uk>
2022-07-04 18:56:53 +0100

    Forgot the controller lol

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-
+import { AzureFunctionsController } from "./src/AzureFunctionsController";
 import { CheckAuth, Endpoint, responseFactory, validate, Validate, validateAuth, validationErrorLog } from "./src/Helpers";
 import { Settings, AuthSettings, ValidationError, AuthValidationError, AuthService, FunctionResponse, FunctionResponseBody } from "./src/Interfaces";
 
-export {CheckAuth, Endpoint, Validate, responseFactory, validate, validateAuth, validationErrorLog , Settings, AuthSettings, ValidationError, AuthValidationError, AuthService, FunctionResponse, FunctionResponseBody}
+export {AzureFunctionsController, CheckAuth, Endpoint, Validate, responseFactory, validate, validateAuth, validationErrorLog , Settings, AuthSettings, ValidationError, AuthValidationError, AuthService, FunctionResponse, FunctionResponseBody}
